3622095bbb8c0d15b227bf39d7b31635bf1c39fa
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-05 13:37:16 -0500

    refactor: change views to widgets

--- demo/lib/features/home/presentation/pages/home_page.dart
+++ demo/lib/features/home/presentation/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:demo/core/ui/custom_text_field.dart';
 import 'package:demo/features/home/data/datasources/shoe_service.dart';
 import 'package:demo/features/home/domain/entities/shoe.dart';
-import 'package:demo/features/home/presentation/views/banner_view.dart';
-import 'package:demo/features/home/presentation/views/shoe_list_view.dart';
+import 'package:demo/features/home/presentation/widgets/banner_view.dart';
+import 'package:demo/features/home/presentation/widgets/shoe_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
